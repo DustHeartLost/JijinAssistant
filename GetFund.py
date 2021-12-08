3387496f3447717cbe5760f1e876abb385e4b2ff
DustHeartLost-abchina <3102194154@qq.com>
2021-12-08 18:00:19 +0800

    完成爬取天天基金网所有基金数据以及每一只基金的详细信息

--- GetFund.py
+++ GetFund.py
@@ -1,11 +1,45 @@
 import requests
 import json
+import time
+import re
 
-url1='http://fund.eastmoney.com/data/FundGuideapi.aspx?dt=4&sd=&ed=&sc=3y&st=desc&pi=1&pn=2000000&zf=diy&sh=list&rnd=0.24506401305749903'
-# url = "https://fundgz.1234567.com.cn/js/217011.js"
-response=requests.get(url1)
-text = response.text.split('=')[1]
-data=json.loads(text)
-list=data['datas']
-print(len(list))
-# print(len(list[0].split(','))/24)
+
+class Funds:
+    """基金处理类,主要功能包括：
+        1.获取基金相关信息
+        2.筛选符合条件的基金信息
+    """
+
+    def __init__(self) -> None:
+        """初始化得到所有基金的基本数据
+        """
+        url = 'http://fund.eastmoney.com/data/FundGuideapi.aspx?dt=4&sd=&ed=&sc=3y&st=desc&pi=1&pn=10000&zf=diy&sh=list&rnd=0.24506401305749903'
+        response = requests.get(url)
+        self.fundsList = json.loads(response.text.split('=')[1])['datas']
+
+    def findFund(fundCode):
+        """获取单个基金的基本信息（天天基金网）,并通过正则表达式将其转为可识别的数据
+
+        Args:
+            fundCode (String): 基金代码
+
+        Returns:
+            String: 基金基本信息的Json字符串
+        """
+        v = time.strftime(r'%Y%m%d%H%M%S')
+        url = f"http://fund.eastmoney.com/pingzhongdata/{fundCode}.js?v={v}"
+
+        response = requests.get(url)
+        return response.text
+
+
+if __name__ == "__main__":
+    # TODO: 完善后续的各个字段的取值
+    fundCode = "005669"
+    v = time.strftime(r'%Y%m%d%H%M%S')
+    url = f"http://fund.eastmoney.com/pingzhongdata/{fundCode}.js?v={v}"
+    fundData = requests.get(url).text
+    Data_currentFundManager = re.findall(
+        "var Data_currentFundManager = .;", fundData, 0)
+
+    print(aa)
