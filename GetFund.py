f2897cad797852cbc03424fc71a304d32c95ec98
DustHeartLost-abchina <3102194154@qq.com>
2021-12-13 17:42:21 +0800

    完成查找基金重仓股票的函数

--- GetFund.py
+++ GetFund.py
@@ -31,7 +31,7 @@ class Funds:
             fundCode (String): 基金代码
 
         Returns:
-            String: 基金经理基本信息的Json字符串
+            Dictionary: 基金经理基本信息的字典对象
         """
         v = time.strftime(r'%Y%m%d%H%M%S')
         url = f"http://fund.eastmoney.com/pingzhongdata/{fundCode}.js?v={v}"
@@ -41,14 +41,14 @@ class Funds:
             r"var Data_currentFundManager =([^;]*)", fundData, 0)[0])
         return Data_currentFundManager
 
-    def findFundInformation(self,fundCode):
+    def findFundInformation(self, fundCode):
         """根据基金代码返回基金的基本信息
 
         Args:
             fundCode (String): 基金代码
 
         Returns:
-            Json: 基金基本信息的json字符串
+            Dictionary: 基金基本信息的字典对象
         """
         Data_fundInformation = {}
         url = f"http://fundf10.eastmoney.com/jbgk_{fundCode}.html"
@@ -63,10 +63,48 @@ class Funds:
                 Data_fundInformation[ths[i].text] = tds[i].text
         return Data_fundInformation
 
+    def findFundHeavyStock(self, fundCode):
+        """根据基金代码查找基金的重仓股票
+
+        Args:
+            fundCode (String): 基金代码
+
+        Returns:
+            List: 重仓股票信息的字典对象列表
+        """
+        url = f"http://fundf10.eastmoney.com/FundArchivesDatas.aspx?type=jjcc&code={fundCode}&topline=20"
+
+        fundData = requests.get(url).text
+        html = re.findall(r"content:\"(.*)\",arryear", fundData, 0)[0]
+        table = BeautifulSoup(html, "html.parser").find(
+            "table", attrs={"class": "w782 comm tzxq"})
+        trs = table.find_all("tr")
+        Data_heavyStock = []
+        # 删除标题栏数组里不需要的元素
+        theads = trs[0].find_all("th")
+        del theads[3]
+        del theads[3]
+        del theads[3]
+        del theads[4]
+        del theads[4]
+        for i in range(1,len(trs)):
+            #删除数据里不需要的元素 
+            tds = trs[i].find_all("td")
+            del tds[3]
+            del tds[3]
+            del tds[3]
+            del tds[4]
+            del tds[4]
+            tempData={}
+            for j in range(len(tds)):
+                tempData[theads[j].text] = tds[j].text
+            Data_heavyStock.append(tempData)
+        return Data_heavyStock
+
 
 if __name__ == "__main__":
     # TODO: 完善后续的各个字段的取值
     fundCode = "001595"
     v = time.strftime(r'%Y%m%d%H%M%S')
     fund = Funds()
-    print(fund.findFundManager(fundCode))
+    print(fund.findFundHeavyStock(fundCode))
